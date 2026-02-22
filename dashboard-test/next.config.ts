c82e54baf82b29863545f2f86f07dc0a4bf1c1cf
zohreh moazezi <107259983+zohreh-moazezi@users.noreply.github.com>
2026-02-22 16:29:16 +0330

    deployment-issue-v1

--- dashboard-test/next.config.ts
+++ dashboard-test/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  output:"export"
 };
 
 export default nextConfig;
